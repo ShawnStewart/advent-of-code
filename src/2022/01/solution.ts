b02bdf24ae809a5cf5f64e6a33233538d5f8a4d6
Shawn Stewart <shawn.stewart@quarkworks.co>
2022-12-06 09:55:53 -0600

    day 1

--- src/2022/01/solution.ts
+++ src/2022/01/solution.ts
@@ -1,1 +1,19 @@
-console.log('==== Advent of Code (Day 1) ====')
+import { readFileSync } from 'fs';
+
+const calorieItemsList = readFileSync('src/2022/1/input.txt').toString();
+
+function parseItems(items: string) {
+    return items.split('\n').map(s => parseInt(s, 10));
+}
+
+const calorieItemsPerElf = calorieItemsList.split('\n\n').map(parseItems);
+
+function sum(prev: number, current: number) {
+    return prev + current;
+}
+
+const totalCaloriesPerElf = calorieItemsPerElf.map(calorieItems => calorieItems.reduce(sum, 0)).sort();
+
+const topThree = totalCaloriesPerElf.slice(-3).reduce(sum, 0);
+
+console.log('highest calories', topThree);
